ef857befe699961a3a2a7a01680e0972c5dd4c64
tylerfarnz261 <128645998+tylerfarnz261@users.noreply.github.com>
2024-11-11 21:35:40 -0500

    start changing states to reducers for Contexts

--- src/contexts/users.context.jsx
+++ src/contexts/users.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect  } from "react";
+import { createContext, useEffect, useReducer  } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
 //  Actual value you want to access
@@ -7,8 +7,36 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+const userActionTypes = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const useReducer = (state, action) => {
+    const { type, payload } = action;
+
+
+    switch (type){
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error('Invalid action type');
+    }
+}
+
 export const UserProvider = ({children}) =>{
-    const [currentUser, setCurrentUser] = useState(null);
+    const [{ currentUser }, dispatch] = useReducer(useReducer, {
+        currentUser: null
+    });
+
+    const setCurrentUser = (user) => {
+        dispatch({
+            type: userActionTypes.SET_CURRENT_USER,
+            payload: user
+        })
+    }
     const value = {
         currentUser, setCurrentUser
     };
